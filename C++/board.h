97b38d367cb253327007ad0a0df68009bc88faa7
TJ <tjadeniji@gmail.com>
2020-06-03 19:24:56 -0400

    First Commit, tons of work needed.

---
+++ C++/board.h
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class board
+{
+    private: 
+        int playerScore = 0;
+        int gameScore = 0;
+
+    void DisplayBoard(char row1[], char row2 [], char row3[], string playerName, int playerScore);
+    
+    public: 
+
+     
+
+};
+
+void DisplayBoard(char row1[], char row2[], char row3[], string playerName, int playerScore)
+{
+    cout << "\t\t\t\t\t\t Score: \n";
+    cout << "\t\t\t\t\t\t " + playerName + ": " +  ;
+    cout << "\t\t\t Tic-Tac-Toe"; 
+}
+
+
+//Taking a stab at a side-project. Open to any suggestions and ideas. 
+//
---
+++ C++/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "board.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+
+//Taking a stab at a side-project. Open to any suggestions and ideas. 
+//
