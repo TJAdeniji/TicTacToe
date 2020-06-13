27ed68aff2a7ae905073097968822e2c5d7aa7d6
TJ <tjadeniji@gmail.com>
2020-06-12 21:15:26 -0400

    Added board logic and class functions.

--- C++/board.h
+++ C++/board.h
@@ -7,20 +7,54 @@ class board
     private: 
         int playerScore = 0;
         int gameScore = 0;
+        char player = ' ';
+        char computer = ' ';
 
-    void DisplayBoard(char row1[], char row2 [], char row3[], string playerName, int playerScore);
+        int getPlayerScore (void);
+        int getGameScore (int gameScore);
+        char setPlayerLetter(char player);
     
     public: 
-
-     
+    void DisplayBoard(string playerName, char headsOrTails);
+    char playerFirst();
+    char computerFirst();
+        
 
 };
 
-void DisplayBoard(char row1[], char row2[], char row3[], string playerName, int playerScore)
+int board::getPlayerScore(void)
 {
+    return playerScore;
+}
+
+char board::setPlayerLetter(char player)
+{
+    player = this->player;
+}
+
+void board::DisplayBoard(string playerName, char headsOrTails) 
+{
+     
+    char board[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     cout << "\t\t\t\t\t\t Score: \n";
-    cout << "\t\t\t\t\t\t " + playerName + ": " +  ;
-    cout << "\t\t\t Tic-Tac-Toe"; 
+    cout << "\t\t\t\t\t\t " << playerName << ": " << playerScore;
+    cout << "\t\t\t Tic-Tac-Toe";  
+    
+    cout<< "tt     |     |     \n";
+    cout<< "tt  " << board[0][0] << "  | " << board[0][1] << "  |  " << board[0][2]<< " \n";
+    cout<< "tt_____|_____|_____\n";
+    cout<< "tt     |     |     \n";
+    cout<< "tt  " << board[1][0] << "  | " << board[1][1] << "  |  " << board[1][2] << " \n";
+    cout<< "tt_____|_____|_____\n";
+    cout<< "tt     |     |     \n";
+    cout<< "tt  " << board[2][0] << "  | " << board[2][1] << "  |  " << board[2][2] << " \n";
+    cout<< "tt     |     |     \n";
+    
+}
+
+char computerFirst()
+{
+
 }
 
 
--- C++/main.cpp
+++ C++/main.cpp
@@ -5,7 +5,32 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    string playerName;
+    char headsOrTails;
+    board player; 
+
+    cout << "Welcome to Tic-Tac-Toe!!!\n";
+    cout << "What's your name? \n";
     
+    //cin >> playerName;
+    cin >> playerName;  
+
+    //player.setName(playerName);
+
+    cout << "Would you like to go first? Please choose Heads(H) or Tails(T): \n";
+    cin >> headsOrTails;
+    toupper(headsOrTails);
+
+    while (headsOrTails != 'H'|| headsOrTails != 'T')//Determines if the player or game goes first. 
+    {
+        cout << "Incorrect choice. Please enter H or T. \n";
+        cin >> headsOrTails;
+        toupper(headsOrTails);
+    }  
+
+    player.DisplayBoard(playerName, headsOrTails);
+
+
     return 0;
 }
 
